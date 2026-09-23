82353efd90a40ee36897098ec5b72351b26bd8ea
biba-glitch <2230005567@iau.edu.sa>
2026-09-23 15:45:18 +0300

    Add files via upload

---
+++ task2_family.pl
@@ -0,0 +1,20 @@
+% Lab 4 - Task 2: Family facts
+% (as given in the "Variables -- Examples" slide of this lab)
+
+male(ali).
+male(ahmad).
+male(muhammad).
+male(faisal).
+male(hassan).
+male(khan).
+
+female(sara).
+female(nora).
+
+parent(ali, ahmad).
+parent(ali, muhammad).
+parent(ahmad, sara).
+parent(ahmad, nora).
+parent(ahmad, khan).
+parent(muhammad, faisal).
+parent(faisal, hassan).
